799cf52820f3a7eb94d64020ed933361e9157277
Dawwo-15 <davidemongera@hotmail.com>
2023-10-05 20:42:26 +0200

    Added custom inspector for Displacer Component
    Now automatically pushed a notification in the inspector when the 2 arrays are not properly set up

--- Scripts/Displacer.cs
+++ Scripts/Displacer.cs
@@ -1,7 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.TextCore.LowLevel;
 
 public class Displacer : MonoBehaviour
 {
@@ -36,29 +34,29 @@ public class Displacer : MonoBehaviour
         own_transform.rotation = ref_transform.rotation * angular_displacement;
     }
 
-    [ContextMenu("Print operations setup needs")]
-    void CheckForInconsistencies ()
+    public List<int> CheckReferences()
     {
-        foreach (PointDisplacement p in operations)
-        {
-            Debug.Log("Needs Update? " + p.NeedSetup);
-        }
-    }
+        List<int> brokenOperations = new List<int>();
 
-    [ContextMenu("Check References")]
-    void CheckReferences()
-    {
-        bool encountered_problems = false;
+        if (operations.Length != displacement_references.Length)
+        {
+            brokenOperations.Add(1);
+            return brokenOperations;
+        } else
+        {
+            brokenOperations.Add(0);
+        } 
+
         for (int i = 0; i < operations.Length; i++)
         {
             if (operations[i].NeedSetup)
                 if (displacement_references[i] == null)
                 {
-                    Debug.LogWarning("Operation #" + (i+1) + " necessitates a reference object but found none.");
-                    encountered_problems = true;
+                    //Debug.LogWarning("Operation #" + (i+1) + " necessitates a reference object but found none.");
+                    brokenOperations.Add(i);
                 }     
         }
-        if (!encountered_problems)
-            Debug.Log("No problem encountered in setup");
+
+        return brokenOperations;
     }
 }
---
+++ Scripts/Editor/DisplacerCustomEditor.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(Displacer))]
+public class DisplacerCustomEditor : Editor
+{
+    private bool componentHealthStatus = false;
+    private string errorString = "Placeholder Error String";
+    public override void OnInspectorGUI()
+    {
+        Displacer displacer = (Displacer)target;
+        EditorGUI.BeginChangeCheck();
+
+        if (componentHealthStatus)
+            drawWarningBox(errorString);
+
+        base.OnInspectorGUI();
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            List<int> indexes = displacer.CheckReferences();
+            if (indexes[0] == 1)
+            {
+                componentHealthStatus = true;
+                errorString = "\"operations\" array and \"displacement_references\" array have different sizes, please add empty elements or delete from \"displacement_references\" accordingly.";
+            } else
+            {
+                if (indexes.Count > 1)
+                {
+                    componentHealthStatus = true;
+                    errorString = "WARNING: Operations with index: ";
+                    for (int i = 1; i < indexes.Count; i++)
+                    {
+                        errorString += i.ToString();
+                        if (i != indexes.Count - 1)
+                            errorString += ",";
+                    }
+                    errorString += " require a corresponding reference GameObject, but it was not found.";
+                } else
+                {
+                    componentHealthStatus = false;
+                }     
+            }
+        }
+
+        
+    }
+
+    public void drawWarningBox(string errorString)
+    {
+        EditorGUILayout.HelpBox(errorString, MessageType.Error);
+    }
+}
+
+#endif
